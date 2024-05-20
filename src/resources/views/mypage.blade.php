8460381ffb6f748cb13f4b7fd0296ce4179e234a
aruma-joishi <aru-jo@docmo.ne.jp>
2024-05-21 08:55:36 +0900

    CSS

--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -5,40 +5,47 @@
 
 <h1>お気に入り</h1>
 @foreach ($adjustShops as $adjustShop)
-<ul>
-  <li>{{$adjustShop -> image}}</li>
-  <li>{{$adjustShop -> name}}</li>
-  <li>#{{$adjustShop -> area}}</li>
-  <li>#{{$adjustShop -> genre}}</li>
-  <a href="/detail/{{$adjustShop -> id}}">{{$adjustShop -> id}}詳しく見る</a>
-
-</ul>
-@endforeach
-
-
-<h1>予約情報</h1>
-
-@foreach ($reservations as $reservation)
-<h2>予約{{ $loop->index +1 }}</h2>
-<form class="delete-form" action="/delete" method="POST">
-  @method('DELETE')
-  @csrf
-  <div class="delete-form__button">
-    <input type="hidden" name="id" value="{{ $reservation['id'] }}">
-    <button class="delete-form__button-submit" type="submit">削除</button>
+<div class="card__item">
+  <div class="card__img">
+    <img src="{{$adjustShop -> image}}" alt="{{$adjustShop -> image}}">
   </div>
-</form>
-@foreach ($shops as $shop)
-@if($reservation['shop_id'] == $shop['id'])
-<li>{{$shop -> name}}</li>
-@endif
-
+  <div class="card__content">
+    <h2>{{$adjustShop -> name}}</h2>
+    <div class="card__tag">
+      <p>#{{$adjustShop -> area}}</p>
+      <p>#{{$adjustShop -> genre}}</p>
+    </div>
+    <div class="card__footer">
+      <a href="/detail/{{$adjustShop -> id}}">詳しく見る</a>
+    </div>
+  </div>
+</div>
 @endforeach
-<li>{{$reservation -> date}}</li>
-<li>{{$reservation -> time}}</li>
-<li>{{$reservation -> number}}人</li>
 
-</ul>
-@endforeach
 
-@endsection
+      <h1>予約情報</h1>
+
+      @foreach ($reservations as $reservation)
+      <h2>予約{{ $loop->index +1 }}</h2>
+      <form class="delete-form" action="/delete" method="POST">
+        @method('DELETE')
+        @csrf
+        <div class="delete-form__button">
+          <input type="hidden" name="id" value="{{ $reservation['id'] }}">
+          <button class="delete-form__button-submit" type="submit">削除</button>
+        </div>
+      </form>
+      @foreach ($shops as $shop)
+      @if($reservation['shop_id'] == $shop['id'])
+      <li>{{$shop -> name}}</li>
+      @endif
+
+      @endforeach
+      <li>{{$reservation -> date}}</li>
+      <li>{{$reservation -> time}}</li>
+      <li>{{$reservation -> number}}人</li>
+
+      </ul>
+      @endforeach
+
+      @endsection
